7d8edecc4caf9782750a3df148b6064a72016e19
Tigran0199 <cruelman11@gmail.com>
2022-03-29 16:31:07 +0400

    Feat: added data file for all data of project

---
+++ src/components/data/data.js
@@ -0,0 +1,11 @@
+import HEADER_LOGO from "./images/trello-brands.svg"
+import MAIN_PAGE_IMG from "./images/mainPage.png"
+
+const TITLE_NAME = "Trello помогает командам эффективно решать рабочие задачи."
+const TITLE_DESCRIPTION = "Работайте в команде, управляйте проектами и выводите продуктивность на новый уровень собственным уникальным способом вместе с Trello."
+const SECOND_PAGE_TITLE = "Это не просто работа. Это координация действий в команде."
+const SECOND_PAGE_DESCRIPTION = "Начните с досок, колонок и карточек, а затем переходите к более сложным функциям. Управляйте проектами, упорядочивайте задачи и поддерживайте командный дух — все это в Trello."
+
+
+
+export  {HEADER_LOGO , TITLE_NAME , TITLE_DESCRIPTION , MAIN_PAGE_IMG , SECOND_PAGE_TITLE , SECOND_PAGE_DESCRIPTION }
