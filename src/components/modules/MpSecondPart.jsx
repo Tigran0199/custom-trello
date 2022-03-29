2632ad2fc7e02b7f4846b19b43df6768ae64f024
Tigran0199 <cruelman11@gmail.com>
2022-03-29 16:23:52 +0400

    feat: Added css styles for pages

---
+++ src/components/modules/MpSecondPart.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import {}from '../data/data'
+import style from '../styles/SecondPart.module.css'
+
+function MpSecondPard(){
+    return (
+        <div className={style.secondPart} >
+        </div>
+    )
+}
+export default MpSecondPard
