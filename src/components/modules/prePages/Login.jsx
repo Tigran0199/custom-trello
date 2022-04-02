aa428a3d711db643166dd99eee5095db23bbfd02
Tigran0199 <cruelman11@gmail.com>
2022-04-02 17:08:21 +0400

    feat: Added validation for users login

--- src/components/modules/prePages/Login.jsx
+++ src/components/modules/prePages/Login.jsx
@@ -1,26 +1,55 @@
-import React from 'react';
+import React , {useState,useEffect} from 'react';
 import styles from '../../styles/LoginPage.module.css'
 import {useNavigate} from "react-router-dom";
 
 function Login() {
-    const navigation = useNavigate();
+    const navigate = useNavigate("")
+    const [email , setEmail] = useState("")
+    const [pass , setPass] = useState("")
+    const [users , setUsers] = useState({})
+    const passChanger= (event)=>{
+        setPass(event.target.value)
+    }
+    const mailChanger= (event)=>{
+        setEmail(event.target.value)
+    }
+    function userLogin(){
+        if(!email && !pass){
+            alert("Invalid Input")
+        }
+        if(users[email]&& users[email]["pass"]===pass){
+          localStorage.setItem("isLoggedIn" , true)
+          navigate("/boards")
+        }
+    }
+
+    useEffect(() => {
+        fetch("https://620367724d21c200170b9ccd.mockapi.io/countries")
+            .then((response)=>{
+                return response.json()
+            })
+            .then((data)=>{
+               setUsers(data[0])
+            })
+
+    }, []);
   return (
     <section className={styles.loginSection}>
      <div className={styles.firstIllustration}>
          <button className={styles.back} onClick={()=>{
            window.history.back();
          }}
-         ></button>
+         />
      </div>
      <div className={styles.loginBlock}>
        <h1>Login to Trello</h1>
-         <input className={styles.inputs} type="email" placeholder="E-mail" />
-         <input className={styles.inputs} type="password" placeholder="Password"/>
+         <input value={email} onChange={mailChanger} className={styles.inputs} type="email" placeholder="E-mail" />
+         <input value={pass}  onChange={passChanger} className={styles.inputs} type="password" placeholder="Password"/>
          <button className={styles.button17} onClick={()=>{
-            navigation('/boards');
+             userLogin()
          }}>Login</button>
      </div>
-     <div className={styles.secondIllustration}></div>
+     <div className={styles.secondIllustration}/>
     </section>
   );
 }
